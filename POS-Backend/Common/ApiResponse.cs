416dbcc7f4fcfd6095bc9a3b968d7bda9ab663c3
TeddyToT <minh.bakuryu@gmail.com>
2025-12-17 13:07:43 +0700

    feat(backend): add common ApiResponse, controller ProductController and builder

---
+++ POS-Backend/Common/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace POS_Backend.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public int? Total { get; set; }
+        public int? Page {  get; set; }
+        public int? Limit { get; set; }
+        public int? TotalPages { get; set; }
+    }
+}
--- POS-Backend/Controllers/ProductsController.cs
+++ POS-Backend/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using POS_Backend.Models;
+using POS_Backend.Services;
+using POS_Backend.Common;
 
 namespace POS_Backend.Controllers
 {
@@ -6,6 +9,33 @@ namespace POS_Backend.Controllers
     [Route("api/products")]
     public class ProductsController : Controller
     {
+        private readonly ProductService _productService;
 
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetProducts(
+            [FromQuery] int page = 1,
+            [FromQuery] int limit = 10)
+        {
+            var (items, total) = _productService.GetProducts(page, limit);
+            var totalPages = (int)Math.Ceiling(total / (double)limit);
+
+            var response = new ApiResponse<List<Product>>
+            {
+                Success = true,
+                Message = "Products retrieved successfully",
+                Data = items,
+                Total = total,
+                Page = page,
+                Limit = limit,
+                TotalPages = totalPages
+            };
+
+            return Ok(response);
+        }
     }
+            
 }
