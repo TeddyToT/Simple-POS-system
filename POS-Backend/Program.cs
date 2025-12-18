b2972264b2f0bf54060727c24600fc32263d038a
TeddyToT <minh.bakuryu@gmail.com>
2025-12-18 13:58:03 +0700

    feat(be-cors): add cors for Vite

--- POS-Backend/Program.cs
+++ POS-Backend/Program.cs
@@ -7,6 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173" // Vite React
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials(); 
+    });
+});
+
 
 builder.Services.AddSignalR();
 
@@ -24,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
